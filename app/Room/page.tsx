2270bcc2606d21e52212f8cc9f3af447f015aefc
ishimaruryouto <2240006@ecc.ac.jp>
2025-11-03 12:27:02 +0900

    付箋コード途中

---
+++ app/Room/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Room_create() {
+    return (
+        <>
+            <h1>チャット欄</h1>
+            <div className="">
+                <div className="relative flex items-center w-[150px] h-[150px] px-[2.2em] py-[2em] bg-[#fdf6af] text-[#333]
+            before:absolute before:bottom-[-5px] before:right-[7px] before:-z-10 before:rotate-[5deg]
+            before:w-[70%] before:h-[50%] before:bg-[#d0d0d0] before:blur-[4px] before:content-['']">
+                    <textarea name="text" id="" placeholder="ここに書き込み" className="w-full h-full bg-transparent resize-none outline-none text-sm"></textarea>
+                </div>
+
+                <div className="relative flex items-center w-[150px] h-[150px] px-[2.2em] py-[2em] bg-[#fdf6af] text-[#333]
+            before:absolute before:bottom-[-5px] before:right-[7px] before:-z-10 before:rotate-[5deg]
+            before:w-[70%] before:h-[50%] before:bg-[#d0d0d0] before:blur-[4px] before:content-['']">
+                    <textarea name="text" id="" placeholder="ここに書き込み" className="w-full h-full bg-transparent resize-none outline-none text-sm"></textarea>
+                </div>
+
+                <div className="relative flex items-center w-[150px] h-[150px] px-[2.2em] py-[2em] bg-[#fdf6af] text-[#333]
+            before:absolute before:bottom-[-5px] before:right-[7px] before:-z-10 before:rotate-[5deg]
+            before:w-[70%] before:h-[50%] before:bg-[#d0d0d0] before:blur-[4px] before:content-['']">
+                    <textarea name="text" id="" placeholder="ここに書き込み" className="w-full h-full bg-transparent resize-none outline-none text-sm"></textarea>
+                </div>
+
+                <div className="relative flex items-center w-[150px] h-[150px] px-[2.2em] py-[2em] bg-[#fdf6af] text-[#333]
+            before:absolute before:bottom-[-5px] before:right-[7px] before:-z-10 before:rotate-[5deg]
+            before:w-[70%] before:h-[50%] before:bg-[#d0d0d0] before:blur-[4px] before:content-['']">
+                    <textarea name="text" id="" placeholder="ここに書き込み" className="w-full h-full bg-transparent resize-none outline-none text-sm"></textarea>
+                </div>
+            </div>
+        </>
+    );
+}
