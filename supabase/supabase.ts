533bfcab15650b8b324629abcd95037ef24dd34f
ishimaruryouto <2240006@ecc.ac.jp>
2025-11-17 12:18:43 +0900

    ブラッシュアップ

--- supabase/supabase.ts
+++ supabase/supabase.ts
@@ -1,7 +1,8 @@
-// lib/supabase.ts
+// supabase/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = "https://izvuusjevnpdjvljmkxp.supabase.co";
+const supabaseAnonKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6dnV1c2pldm5wZGp2bGpta3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMjIwMjMsImV4cCI6MjA3ODg5ODAyM30.Ut4XShz9eLB_eltKhXJsl7Qw1nM2NP1UHiOZ7B_xt2c";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
